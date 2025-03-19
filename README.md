c925ca1309c6926c74785c0eda6dfffb5f58d1ec
Alex Cho <alexcho@bytedance.com>
2025-03-19 16:23:10 -0700

    finished the main logic

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# aiagent_from_scratch
+# aiagent_from_scratch_sorta
+
+Practice project to set up basic AI agent from scratch (sorta) using Anthropic API
+
+# Learnt
+
+Creating an AI Agent using Anthropic API, having AI Agent use tools.
+
+# Demo
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -15,4 +16,36 @@ class ResearchResponse(BaseModel):
     tools_used: list[str]
 
 response = llm.invoke("What is the meaning of life")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What is philosophy? ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
