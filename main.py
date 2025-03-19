0a05f1b31684fb26b8540edf0e78b33ae04a7947
Alex Cho <alexcho@bytedance.com>
2025-03-19 16:24:06 -0700

    added tools to main ai agent logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
@@ -34,10 +35,11 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools = [search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
